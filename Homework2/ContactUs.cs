50c567e7b3a0f6ab75cf9ac8763c4b61478271e5
Maksym Yankovskyi <maks.yankovskiy27@gmail.com>
2023-04-03 12:18:16 +0300

    add homework2

---
+++ Homework2/ContactUs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    public class ContactUs
+    {
+        public void ShowContact()
+        {
+            Console.WriteLine("DIC Building @4, Al Sufouh - Al Sufouh 2 - Dubai");
+            Console.WriteLine("Business hours:");
+            Console.WriteLine("Monday - Friday");
+            Console.WriteLine("7:30am - 4:30pm.");
+            Console.WriteLine("Phone: +971 4 391 1111");
+        }
+    }
+}
---
+++ Homework2/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    public class Product
+    {
+        private string _nameProduct;
+        private int _priceOfProduct;
+        public string NameProduct
+        {
+            get { return _nameProduct; }
+        }
+
+        public int PriceOfProduct
+        {
+            get { return _priceOfProduct; }
+        }
+
+        public Product(string nameProduct, int priceOfProduct)
+        {
+            _nameProduct = nameProduct;
+            _priceOfProduct = priceOfProduct;
+        }
+    }
+}
